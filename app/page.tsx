66035bfe69a742ed26bcb47a70461ee0ede3fab9
Pich <pichouk577@gmail.com>
2024-12-12 16:17:05 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Nav } from "@/components/nav";
+import Image from "next/image";
+
+
+
+export default function Home() {
+  return (
+    <body className="bg-gradient-to-r from-white from-50% to-cyan-200">
+      <Nav />
+    </body>
+
+  );
+}
