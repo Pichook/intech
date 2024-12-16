62a7dc5b942d4774f9938c243b01a0a52e666430
Pich <pichouk577@gmail.com>
2024-12-16 17:38:25 +0700

    reverting

--- app/page.tsx
+++ app/page.tsx
@@ -12,33 +12,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <html>
-      <head>
-        {/* Add your Google Fonts link here */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body>
-        <div className="grid grid-flow-row ">
-          <Nav />
-          <Frame1 />
-          <Frame2 />
-          <Frame3 />
-          <Frame4 />
-          <Frame5 />
-          <div className="bg-white">
-            <Frame6 />
-            <hr className="border-t border-[#FBB03B] mt-4" />
-            <Bottom />
-          </div>
+    <div className="grid grid-flow-row ">
+      <Nav />
+      <Frame1 />
+      <Frame2 />
+      <Frame3 />
+      <Frame4 />
+      <Frame5 />
+      <div className="bg-white">
+        <Frame6 />
+        <hr className="border-t border-[#FBB03B] mt-4" />
+        <Bottom />
+      </div>
 
-        </div>
-      </body>
-    </html>
-
-
+    </div>
     
 
   );
